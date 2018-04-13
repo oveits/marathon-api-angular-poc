d67da6bbddd3057af0e8cfd960b7c8f4aea6f941
Oliver Veits <oliver.veits@web.de>
2018-04-13 20:10:25 +0200

    added list of app IDs (successful)and testing pipes (unsuccessful)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TimerObservable } from "rxjs/observable/TimerObservable";
 import 'rxjs/add/operator/takeWhile';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Component({
@@ -12,14 +13,20 @@ import 'rxjs/add/operator/takeWhile';
 export class AppComponent implements OnInit, OnDestroy {
   app = 'myapp';
   app_id = 'unknown';
+  myapp: App;
   deployments = 'unknown';
   tasksStaged = 'unknown';
   tasksHealthy = 'unknown';
   tasksUnhealthy = 'unknown';
   tasksRunning = 'unknown';
   httpOptions = null;
-  intervalTimerMsec = 2500;
-  private alive: boolean = true;
+  intervalTimerMsec = 25000000;
+  private alive : boolean = true;
+  private marathonURL : String = "http://94.130.187.229/service/marathon/v2";
+  //marathonApps : Observable<App[]> = null;
+  marathonApps : App[] = [{"id": 1}];
+  myApps = null;
+  token = 'eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzeUY1VE9TemRsSTQ1UTF4c3B4emVvR0JlOWZOeG05bSIsImVtYWlsIjoib2xpdmVyLnZlaXRzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE1MjM5ODk4NTMsImlhdCI6MTUyMzU1Nzg1MywiaXNzIjoiaHR0cHM6Ly9kY29zLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNjI1MzMxNzc0ODE4NzQ5MDc3NCIsInVpZCI6Im9saXZlci52ZWl0c0BnbWFpbC5jb20ifQ.NFUW50Gzl78qfI99OPuM6YrxfU4OYLhzWQz7kfoEJPY';
 
   constructor(private _http: HttpClient) {
   }
@@ -28,17 +35,44 @@ export class AppComponent implements OnInit, OnDestroy {
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
-        'Authorization': 'token=eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzeUY1VE9TemRsSTQ1UTF4c3B4emVvR0JlOWZOeG05bSIsImVtYWlsIjoib2xpdmVyLnZlaXRzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE1MjM1NTM0ODQsImlhdCI6MTUyMzEyMTQ4NCwiaXNzIjoiaHR0cHM6Ly9kY29zLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNjI1MzMxNzc0ODE4NzQ5MDc3NCIsInVpZCI6Im9saXZlci52ZWl0c0BnbWFpbC5jb20ifQ.0mM9qaNenUgUnT3NZS71_9reLTojKRdGaqRzTOO-X4I'
+        'Authorization': 'token=' + this.token
       })
     };
 
+/*
+    this.listMarathonAppsService()
+          .subscribe(
+            res => {
+              this.marathonApps = res;
+              console.log(res);
+            },
+            err => {
+              console.log("Error occured");
+            }
+          );
+*/
+
+    this.myApps = this.getAppsWithPipe();
+
+    this.getApps().subscribe(
+            res => { 
+              console.log('getApps res:'); 
+              console.log(res.apps); 
+              this.marathonApps = res.apps; 
+              console.log('this.marathonApps follows:');
+              console.log(this.marathonApps);
+    });
 
     TimerObservable.create(0, this.intervalTimerMsec)
       .takeWhile(() => this.alive)
       .subscribe(() => {
-        this.getMarathonAppsService()
+        this.getMarathonAppsService('mynamespace', 'nginx-hello-world-service')
           .subscribe(
             res => {
+              this.myapp = <App>res.app;
+              console.log('myapp.id = ' + this.myapp.id);
+              console.log(this.myapp);
+              console.log('myapp.disk = ' + this.myapp.disk);
               this.app_id = res.app.id;
               this.deployments = res.app.deployments.length;
               this.tasksStaged = res.app.tasksStaged;
@@ -62,15 +96,55 @@ export class AppComponent implements OnInit, OnDestroy {
     this.alive = false;
   }
 
-  getMarathonAppsService(){
-    return this._http.get<any>(
-      'http://94.130.187.229/service/marathon/v2/apps/mynamespace/nginx-hello-world-service',this.httpOptions) ;
+  getAppsWithPipe(): Observable<App[]>{
+    return this._http.get<App[]>(this.marathonURL + '/' + 'apps' + '/', this.httpOptions)
+      .pipe(
+        tap(),
+        //map(res => this.myApps = res),
+        //tap(apps => this.log('fetched apps')) //,
+        //tap(apps => this.marathonApps = apps; ),
+        //catchError(this.handleError('getApps', [])
+       )
+      );
   }
 
-  setInstances( runningInstances : Integer ) {
+  getApps(): Observable<App[]>{
+    return this._http.get<App[]>(this.marathonURL + '/' + 'apps' + '/', this.httpOptions);
+  }
+
+  getMarathonAppsService( namespace : String, serviceName : String ){
+    return this._http.get(
+      this.marathonURL + '/' + 'apps' + '/' + namespace + '/' + serviceName, this.httpOptions) ;
+      //'http://94.130.187.229/service/marathon/v2/apps/mynamespace/nginx-hello-world-service',this.httpOptions) ;
+  }
+
+  patchMarathonAppsService( namespace : String, serviceName : String, body : Object ){
+    return this._http.patch<any>(
+      this.marathonURL + '/' + 'apps', 
+      body,
+      this.httpOptions) ;
+  }
+
+  setInstances( namespace : String, serviceName : String, runningInstances : Integer ) {
+   //this.deployments = this.deployments + 1;
+   var body = [{"id": "/" + namespace + "/" + serviceName,"instances": runningInstances}];
+   this.patchMarathonAppsService( 'mynamespace', 'nginx-hello-world-service', body )
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+  }
+
+  setInstancesOld( runningInstances : Integer ) {
+   var body = [{"id": "/mynamespace/nginx-hello-world-service","instances": runningInstances}];
    this._http.patch<any>(
       'http://94.130.187.229/service/marathon/v2/apps', 
-      [{"id": "/mynamespace/nginx-hello-world-service","instances": runningInstances}], this.httpOptions)
+      //[{"id": "/mynamespace/nginx-hello-world-service","instances": runningInstances}], this.httpOptions)
+      body, this.httpOptions)
       .subscribe(
         res => {
           console.log(res);
@@ -181,5 +255,15 @@ this.httpOptions
     )
 
   }
+
+  interface App {
+    id: String;
+    deployments: String;
+    tasksStaged: String;
+    tasksHealthy: String;
+    tasksUnhealthy: String;
+    tasksRunning: String;
+  }
+
 
 }
