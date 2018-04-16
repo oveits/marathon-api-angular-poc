9b0b1be867db30025349920b0cb37c83915dbaa0
Oliver Veits <oliver.veits@web.de>
2018-04-17 00:01:37 +0200

    Added button for adding apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent implements OnInit, OnDestroy {
   //restItems : RestItem[] = null;
   restItemsConfigured = null;
   //ticks = 0;
+  updateAlways=true;
   token = 'eyJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzeUY1VE9TemRsSTQ1UTF4c3B4emVvR0JlOWZOeG05bSIsImVtYWlsIjoib2xpdmVyLnZlaXRzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE1MjM5ODk4NTMsImlhdCI6MTUyMzU1Nzg1MywiaXNzIjoiaHR0cHM6Ly9kY29zLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNjI1MzMxNzc0ODE4NzQ5MDc3NCIsInVpZCI6Im9saXZlci52ZWl0c0BnbWFpbC5jb20ifQ.NFUW50Gzl78qfI99OPuM6YrxfU4OYLhzWQz7kfoEJPY';
 
   constructor(private _http: HttpClient) {
@@ -56,7 +57,7 @@ export class AppComponent implements OnInit, OnDestroy {
                   if (this.restItemsConfigured == null && this.restItems != null) { 
                     this.restItemsConfigured = this.restItems; 
                   }
-                  if (this.restItemsConfigured && this.restItems) {
+                  if (this.restItemsConfigured && this.restItems && this.updateAlways) {
                     this.updateRestItemsAll();
                   }
                   console.log('this.restItems follows:');
@@ -154,12 +155,11 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
 
-  postRestItemsService(){
-    return this._http.post<any>(
-      //'http://94.130.187.229/service/marathon/v2/apps/mynamespace/nginx-hello-world-service', 
-      'http://http://195.201.30.230:4200/service/marathon/v2/apps/mynamespace/nginx-hello-world-service', 
+  putRestItemsService( id : String ){
+    console.log("putRestItemsService(id = " + id + ")");
+    return this._http.put<any>(this.restURL + "/" + id,
 {
-  "id": "/mynamespace/nginx-hello-world-service2",
+  "id": id,
   "backoffFactor": 1.15,
   "backoffSeconds": 1,
   "container": {
@@ -168,11 +168,9 @@ export class AppComponent implements OnInit, OnDestroy {
         "containerPort": 80,
         "hostPort": 0,
         "labels": {
-          "VIP_0": "/mynamespace/nginx-hello-world-service2:80"
         },
         "protocol": "tcp",
-        "servicePort": 80,
-        "name": "mynamespace-nginx"
+        "servicePort": 80
       }
     ],
     "type": "DOCKER",
@@ -199,13 +197,13 @@ export class AppComponent implements OnInit, OnDestroy {
       "path": "/"
     }
   ],
-  "instances": 3,
+  "instances": this.findRestItemById(id) ? this.findRestItemById(id).instances : 1,
   "labels": {
-    "HAPROXY_DEPLOYMENT_GROUP": "nginx-hostname2",
+    "HAPROXY_DEPLOYMENT_GROUP": "nginx-hostname",
     "HAPROXY_0_REDIRECT_TO_HTTPS": "false",
     "HAPROXY_GROUP": "external",
     "HAPROXY_DEPLOYMENT_ALT_PORT": "80",
-    "HAPROXY_0_PATH": "/mynamespace/nginx2",
+    "HAPROXY_0_PATH": id,
     "HAPROXY_0_VHOST": "195.201.17.1"
   },
   "maxLaunchDelaySeconds": 3600,
@@ -230,8 +228,15 @@ export class AppComponent implements OnInit, OnDestroy {
   "constraints": []
 }
 ,
-this.httpOptions
-    )
+this.httpOptions)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
 
   }
 
